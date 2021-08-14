b29c7f05f2ce4c7dbbc81b283dec670d746488d5
Osotatanekeeni <tatanekari52@gmail.com>
2021-08-14 17:56:05 +0200

    Updated with dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,29 +18,52 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     /**
      * Roll the dice and update the screen with the result
      */
     private fun rollDice() {
-        //val resultEditView: EditText = findViewById(R.id.selectSides) // to enter number of sides... makes no sense cause a dice only has 6 sides... RIGHT?! :O
-        // val sides = resultEditView.text.toString()
-
         // Create a new dice object with 6 sides and roll it
         val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val diceRoll1 = dice.roll()
         val diceRoll2 = dice.roll()
-        var total = diceRoll + diceRoll2
+        val total = diceRoll1 + diceRoll2
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.dice1)
-        val resultTextView2: TextView = findViewById(R.id.dice2)
+        // Find the ImageView in the layout
+        val diceImage1: ImageView = findViewById(R.id.dice1)
+        val diceImage2: ImageView = findViewById(R.id.dice2)
         val totalView: TextView = findViewById(R.id.total)
 
-        resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll2.toString()
+        // Determine which drawable resource ID to use based on the dice toll
+        val drawableResource1 = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
         totalView.text = total.toString()
+
+        // Update the content description
+        diceImage1.contentDescription = diceRoll1.toString()
+        diceImage1.contentDescription = diceRoll2.toString()
         
     }
 }
